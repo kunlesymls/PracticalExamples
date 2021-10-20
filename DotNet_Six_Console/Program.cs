1da37f270b2b1c21c89f582b86c9f0132fba2e55
AJILEYE JOSEPH <kunlesymls@hotmail.com>
2021-10-20 09:52:45 +0100

    Adding .net 6 console application

---
+++ DotNet_Six_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+
+
+var builder = new StringBuilder();
+
+int a = 4;
+int b = 5;
+Console.WriteLine(a + b);
